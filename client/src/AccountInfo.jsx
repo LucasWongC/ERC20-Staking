9b60241632349eda28f9e1a6c6300211dee317b9
LucasWongC <wm111898@gmail.com>
2021-05-14 10:20:23 -0400

    update AccountInfo

--- client/src/AccountInfo.jsx
+++ client/src/AccountInfo.jsx
@@ -1,71 +1,65 @@
 import React, { Component } from "react";
 
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 
-
-import BigNumber from 'big-number'
+import BigNumber from "big-number";
 
 import { Trans } from "react-i18next";
 
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
+const styles = (theme) => ({
+  box: {
+    color: "orange",
+    colorDefault: "black",
+    background: "#2e3b55d6",
+    borderRadius: 15,
+    border: 2,
+    padding: 20,
+    margin: 0,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  },
+});
 
-const styles = theme => ({
-    box: {
-        color: 'orange',
-        colorDefault: 'black',
-        background : '#2e3b55d6',
-        borderRadius: 15,
-        border: 2,
-        padding: 20, 
-        margin: 0, 
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    },
-  });
+class StakeInfo extends Component {
+  constructor(props) {
+    super(props);
 
-class StakeInfo extends Component{
+    this.state = {
+      viewAmount: 0,
+      referral: props.referral,
+      amountStaked: props.amountStaked,
+      numberOfStake: props.stakeNumber,
+      tokenDecimals: props.tokenDecimals,
+      hasReferral: true,
+    };
 
-    constructor(props){
-        super(props);
+    console.log(props);
+  }
 
-        this.state = {
-            viewAmount: 0,
-            referral: props.referral,
-            amountStaked: props.amountStaked,
-            numberOfStake: props.stakeNumber,
-            tokenDecimals: props.tokenDecimals,
-            hasReferral: true
-        }
+  componentDidMount() {
+    let _amount = BigNumber(this.props.amountStaked);
+    let decimals = BigNumber(10).power(this.state.tokenDecimals);
+    _amount = _amount.divide(decimals);
 
-        console.log(props)
+    let _referral = "";
+
+    if (this.props.referral === "") {
+      this.setState({
+        referral: "No",
+        hasReferral: false,
+      });
     }
 
+    this.setState({
+      viewAmount: _amount.toString(),
+    });
+  }
 
-    componentDidMount(){
-        let _amount = BigNumber(this.props.amountStaked);
-        let decimals = BigNumber(10).power(this.state.tokenDecimals)
-        _amount = _amount.divide(decimals)
-
-        
-        let _referral = ""
-
-        if(this.props.referral === ""){
-            this.setState({
-                referral: "No",
-                hasReferral: false
-            })
-        }
-
-        this.setState({
-            viewAmount: _amount.toString(),
-        })
-
-    }
-
-/*
+  /*
 
                 <Grid item>
                     {this.state.referer === "0x0000000000000000000000000000000000000001" ? (
@@ -86,39 +80,34 @@ class StakeInfo extends Component{
 
 */
 
+  render() {
+    const { classes } = this.props;
 
-    render(){
-
-        const { classes } = this.props;
-
-
-        return(
-            <Paper
-            elevation={4}
-            className={classes.box}>
-                    <Typography variant="body1" component="p"><Trans i18nKey="account_info.totalAmount" />: <b>{this.state.viewAmount}</b></Typography>
-                    <Typography variant="body1" component="p"><Trans i18nKey="account_info.numberOfActiveStake" />: <b>{this.state.numberOfStake}</b></Typography>
-                    <Typography variant="body1" component="p">
-                        <Trans i18nKey="account_info.noReferralPresent" />: 
-                            {this.state.hasReferral ? (
-                                <TextField value={this.state.referral}></TextField>
-                            ) : (
-                                <b> No</b>
-                            )}
-
-                        </Typography>
-
-            </Paper>
-        )
-
-    }
-
-
+    return (
+      <Paper elevation={4} className={classes.box}>
+        <Typography variant="body1" component="p">
+          <Trans i18nKey="account_info.totalAmount" />:{" "}
+          <b>{this.state.viewAmount}</b>
+        </Typography>
+        <Typography variant="body1" component="p">
+          <Trans i18nKey="account_info.numberOfActiveStake" />:{" "}
+          <b>{this.state.numberOfStake}</b>
+        </Typography>
+        <Typography variant="body1" component="p">
+          <Trans i18nKey="account_info.noReferralPresent" />:
+          {this.state.hasReferral ? (
+            <TextField value={this.state.referral}></TextField>
+          ) : (
+            <b> No</b>
+          )}
+        </Typography>
+      </Paper>
+    );
+  }
 }
 
-
 StakeInfo.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(StakeInfo);
