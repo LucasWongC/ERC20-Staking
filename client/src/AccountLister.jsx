8b315973ed37f5bbea3174e533941c80079ae043
LucasWongC <wm111898@gmail.com>
2021-05-14 10:30:23 -0400

    update AccountLister

--- client/src/AccountLister.jsx
+++ client/src/AccountLister.jsx
@@ -1,67 +1,47 @@
 import React, { Component } from "react";
 import getWeb3 from "./utils/getWeb3";
 
+class AccountLister extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      accounts: props.accounts,
+      selectedAccount: props.accounts[0],
+      callback: props.callback,
+    };
 
-class AccountLister extends Component{
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    constructor(props){
-        super(props);
+  handleChange = (event) => {
+    let selectedValue = event.target.value;
+    this.props.callback(selectedValue);
+  };
 
-        this.state = {
-            accounts: props.accounts,
-            selectedAccount: props.accounts[0],
-            callback: props.callback
-        }
+  handleSubmit(event) {
+    event.preventDefault();
 
+    this.state.callback(event.target.value);
+  }
 
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+  render() {
+    const accountList = this.state.accounts.map((account) => (
+      <option key={account} value={account}>
+        {account}
+      </option>
+    ));
 
-    }
-
-
-    handleChange = (event) =>{
-        let selectedValue = event.target.value;
-        this.props.callback(selectedValue);
-    }
-
-
-    handleSubmit(event){
-        event.preventDefault();
-
-        this.state.callback(event.target.value)
-    }
-
-
-
-
-    render(){
-
-        const accountList = this.state.accounts.map((account) =>
-            <option
-                key={account}
-                value={account}>
-                {account}
-            </option>
-
-        );
-
-
-
-        return (
-            <div>
-                <select name="accountSelector" onChange={this.handleChange}>
-                    <option>Select Account where you want to withdraw</option>
-                    {accountList}
-                </select>
-            </div>
-        );
-
-    }
-
-
+    return (
+      <div>
+        <select name="accountSelector" onChange={this.handleChange}>
+          <option>Select Account where you want to withdraw</option>
+          {accountList}
+        </select>
+      </div>
+    );
+  }
 }
-
 
 export default AccountLister;
