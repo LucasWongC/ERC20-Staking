3fc3f2bb9e2fb0ccba8d04055fb6e0a14f4b52b8
LucasWongC <wm111898@gmail.com>
2021-07-14 10:18:19 -0400

    update WithdrawBox

--- client/src/WithdrawBox.jsx
+++ client/src/WithdrawBox.jsx
@@ -1,130 +1,118 @@
 import React, { Component } from "react";
 
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid'
-import CircularProgress from '@material-ui/core/CircularProgress';
-
-
-import PenaltyWithdrawDialog from './PenaltyWithdrawDialog'
-
-import { withSnackbar } from 'notistack';
+import { withSnackbar } from "notistack";
 import { Trans } from "react-i18next";
 
+class WithdrawBox extends Component {
+  constructor(props) {
+    super(props);
 
-class WithdrawBox extends Component{
+    this.state = {
+      contractInstance: props.contractInstance,
+      accounts: props.accounts,
+      stakeID: props.stakeID, //The ID of the stake in the contract
+      amountStaked: 0,
+      referralAddress: 0,
+      withdrawPenalty: 0,
+      selectedAccount: props.accounts[0],
+      withdrawCallback: props.withdrawCallback,
+      loading: false,
+      elasped: true,
+    };
 
-    constructor(props){
-        super(props);
+    this.handlePenaltyWithdraw = this.handlePenaltyWithdraw.bind(this);
+    this.handleDialogClose = this.handleDialogClose.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setAccount = this.setAccount.bind(this);
+  }
 
-        this.state = {
-            contractInstance: props.contractInstance,
-            accounts: props.accounts,
-            stakeID: props.stakeID,     //The ID of the stake in the contract
-            amountStaked: 0,
-            referralAddress: 0,
-            withdrawPenalty: 0,
-            selectedAccount: props.accounts[0],
-            withdrawCallback: props.withdrawCallback,
-            loading: false,
-            elasped: true
-        }
+  handleSubmit(event) {
+    event.preventDefault();
 
+    this.withdrawTokens();
+  }
 
-        this.handlePenaltyWithdraw = this.handlePenaltyWithdraw.bind(this)
-        this.handleDialogClose = this.handleDialogClose.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.setAccount = this.setAccount.bind(this)
-    }
+  withdrawTokens() {
+    this.setState({ loading: true });
+    this.state.contractInstance.methods
+      .returnTokens(this.state.stakeID)
+      .send({ from: this.state.selectedAccount })
+      .then((result) => {
+        this.setState({ loading: false });
+        console.log(result);
 
+        if (this.isWithdrawed(this.state.stakeID)) {
+          console.log("Token successfullu withdrawed");
 
+          this.state.withdrawCallback();
 
-
-    handleSubmit(event){
-        event.preventDefault();
-
-        this.withdrawTokens();
-
-    }
-
-
-
-    withdrawTokens(){
-
-        this.setState({loading: true})
-        this.state.contractInstance.methods.returnTokens(this.state.stakeID).send({from: this.state.selectedAccount}).then( (result) => {
-
-            this.setState({loading: false})
-            console.log(result)
-
-            if(this.isWithdrawed(this.state.stakeID)){
-                console.log("Token successfullu withdrawed")
-
-                this.state.withdrawCallback();
-
-                this.props.enqueueSnackbar(<Trans i18nKey="withdrawBox.tokenSuccesWithdrawMsg" />, {
-                    variant: 'success',
-                    anchorOrigin: {
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    },
-                });
-            }else{
-                console.log("Unable to withdraw tokens, probably the time is not expired yet")
-                this.props.enqueueSnackbar(<Trans i18nKey="withdrawBox.tokenSuccesWithdrawMsg" />, {
-                    variant: 'error',
-                    anchorOrigin: {
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    },
-                });
+          this.props.enqueueSnackbar(
+            <Trans i18nKey="withdrawBox.tokenSuccesWithdrawMsg" />,
+            {
+              variant: "success",
+              anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "left",
+              },
             }
-
-
-        }).catch( (err) => {
-            console.log("Unable to list active stake; " + err)
-            this.setState({loadig: false})
-        });        
-    }
-
-
-    async isWithdrawed(_stakeID){
-        await this.state.contractInstance.methods.getStakeInfo(_stakeID).call().then( (result) => {
-
-            let withdrawed = Boolean(result[2])
-            return withdrawed
-        });
-    }
-
-
-    setAccount(_account){
-        this.setState({selectedAccount: _account});
-    }
-
-
-    handleDialogClose(){
-        this.setState({
-            showPenaltyWarning: false
-        })
-    }
-
-
-    handlePenaltyWithdraw(){
-        this.state.withdrawCallback();
-    }
-
-
-    render(){
-
-        if(this.state.loading){
-            return(
-                <CircularProgress />
-            );
+          );
+        } else {
+          console.log(
+            "Unable to withdraw tokens, probably the time is not expired yet"
+          );
+          this.props.enqueueSnackbar(
+            <Trans i18nKey="withdrawBox.tokenSuccesWithdrawMsg" />,
+            {
+              variant: "error",
+              anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "left",
+              },
+            }
+          );
         }
+      })
+      .catch((err) => {
+        console.log("Unable to list active stake; " + err);
+        this.setState({ loadig: false });
+      });
+  }
 
-        /*
+  async isWithdrawed(_stakeID) {
+    await this.state.contractInstance.methods
+      .getStakeInfo(_stakeID)
+      .call()
+      .then((result) => {
+        let withdrawed = Boolean(result[2]);
+        return withdrawed;
+      });
+  }
+
+  setAccount(_account) {
+    this.setState({ selectedAccount: _account });
+  }
+
+  handleDialogClose() {
+    this.setState({
+      showPenaltyWarning: false,
+    });
+  }
+
+  handlePenaltyWithdraw() {
+    this.state.withdrawCallback();
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <CircularProgress />;
+    }
+
+    /*
                     <AccountLister 
                         accounts={this.state.accounts}
                         callback={this.setAccount}>
@@ -157,28 +145,31 @@ class WithdrawBox extends Component{
 
         */
 
-        return (
-            <Grid 
-                container
-                spacing={1}
-                direction="column"
-                justify="center"
-                alignItems="center"                
-            >
-                    <Grid item>
-                        <Typography color="primary"><Trans i18nKey="withdrawBox.withdrawLabel" /></Typography>
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained" color="primary" onClick={this.handleSubmit}><Trans i18nKey="stake_detail.withdraw" /></Button>
-                    </Grid>
-
-            </Grid>
-        );
-
-    }
-
-
+    return (
+      <Grid
+        container
+        spacing={1}
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item>
+          <Typography color="primary">
+            <Trans i18nKey="withdrawBox.withdrawLabel" />
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleSubmit}
+          >
+            <Trans i18nKey="stake_detail.withdraw" />
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
 }
-
 
 export default withSnackbar(WithdrawBox);
