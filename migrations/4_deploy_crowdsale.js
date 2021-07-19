f859c6435ab94149a7c1bb4f338d6337252aeadc
LucasWongC <wm111898@gmail.com>
2021-07-19 10:23:10 -0400

    update deploy scripts

--- migrations/4_deploy_crowdsale.js
+++ migrations/4_deploy_crowdsale.js
@@ -1,15 +1,14 @@
-var BITNCrowdsale = artifacts.require("./BITNCrowdsale.sol")
-var BitcoinCompanyNetworkTestToken = artifacts.require("./BitcoinCompanyNetworkTestToken.sol");
+var BITNCrowdsale = artifacts.require("./BITNCrowdsale.sol");
+var BitcoinCompanyNetworkTestToken = artifacts.require(
+  "./BitcoinCompanyNetworkTestToken.sol"
+);
 
+module.exports = function (deployer, network, accounts) {
+  var mainnet_address = "0x41ad4093349c8a60de591a3c37dcd184558eaae3";
 
-module.exports = function(deployer, network, accounts){
-
-    var mainnet_address = "0x41ad4093349c8a60de591a3c37dcd184558eaae3"
-    
-    deployer.deploy(BITNCrowdsale, 1, accounts[0], mainnet_address).then( (_Crowdsale) => {
-        console.log("Crowdsale Address: " + BITNCrowdsale.address)
-      });
-    
-
-
-}
+  deployer
+    .deploy(BITNCrowdsale, 1, accounts[0], mainnet_address)
+    .then((_Crowdsale) => {
+      console.log("Crowdsale Address: " + BITNCrowdsale.address);
+    });
+};
